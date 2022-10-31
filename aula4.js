29529c31d1e50b0d1579b6988e38def9c97315c5
Jeferson Santos Oliveira <Jeferson Santos Oliveira>
2022-10-31 13:20:44 -0300

    add the cass 4

---
+++ aula4.js
@@ -0,0 +1,12 @@
+let num1 = 0, num2 = 0, result = 0;
+
+num1 = 10;
+num2 = 20;
+
+//result = num1 + num2;
+//result = num1 * num2;
+//result = num1 / num2;
+//result = num1 ** num2;
+//result = num1 % num2;
+
+console.log(result);
