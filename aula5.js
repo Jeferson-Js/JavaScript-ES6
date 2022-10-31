f3f5bc8a99c1d4383b55112bc1b3df0b8041b551
Jeferson Santos Oliveira <Jeferson Santos Oliveira>
2022-10-31 13:32:23 -0300

    Add the class 5

---
+++ aula5.js
@@ -0,0 +1,13 @@
+let num1 = 10;
+let num2 = 5;
+let num3 = 10;
+
+// > >= < <= ! != == ===
+
+console.log(num1 >= num3); // True
+console.log(num2 >= num3); // False
+console.log(num2 != num3); // Diferente
+console.log(!(num1 >= num3)); //Negação
+console.log(num2 == num3); // False
+console.log(num1 === num3) // True
+console.log(num1 <= num3) // True
